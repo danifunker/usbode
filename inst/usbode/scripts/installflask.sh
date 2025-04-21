8519b40471189176d996bdb1165e749c29bc5e42
Dani Sarfati <daniel.sarfati@lcecorp.com>
2025-04-21 14:30:59 -0400

    cleaning up old code paths

--- inst/usbode/scripts/installflask.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/apt update -y
-/usr/bin/apt install -y python3-flask
--- inst/usbode/usbode.py
+++ inst/usbode/usbode.py
@@ -18,14 +18,7 @@ from pathlib import Path
 from threading import Thread
 import time
 import urllib.parse
-
-try: 
-    from flask import Flask
-except:
-    print("Flask module not found, attempting install...")
-    subprocess.run(['sh', 'scripts/installflask.sh'], cwd="/opt/usbode")
-    print("Flask attempted install, trying to restart to force reload.")
-    quit(1)
+from flask import Flask
 
 store_dev = '/dev/mmcblk0p3'
 store_mnt = '/mnt/imgstore'
@@ -33,7 +26,7 @@ allow_update_from_store = True
 gadgetCDFolder = '/sys/kernel/config/usb_gadget/usbode'
 iso_mount_file = '/opt/usbode/usbode-iso.txt'
 cdemu_cdrom = '/dev/cdrom'
-versionNum = "1.7"
+versionNum = "1.8"
 global updateEvent
 updateEvent = 0
 global fontM
@@ -68,11 +61,11 @@ def getMyIPAddress():
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return f"Welcome to USBODE, the USB Optical Drive Emulator!<br> My IP address is {myIPAddress}. <br> I am currently running from {os.path.abspath(__file__)} .<br>To switch modes click here: <a href='/switch'>/switch</a> <br> Currently Serving: {getMountedCDName()}. <br> Current Mode is: {checkState()} <br> <a href='/list'>Load Another Image</a><br><br>Version Number {versionNum}<br><br><a href='/shutdown'>Shutdown the pi</a>"
+    return f"Welcome to USBODE, the USB Optical Drive Emulator!<br> My IP address is {myIPAddress}. <br>To switch modes click here: <a href='/switch'>/switch</a> <br> Currently Serving: {getMountedCDName()}. <br> Current Mode is: {checkState()} <br> <a href='/list'>Load Another Image</a><br><br>Version Number {versionNum}<br><br><a href='/shutdown'>Shutdown the pi</a>"
 @app.route('/switch')  
 def switch():
     switch()
-    return f'Switching mode... Current mode is {checkState()} (1=CD-Emulator, 2=ExFAT mode)<br><br><a href="/switch">Need to switch modes again?</a><br><br><a href="/setup">Return to Setup</a><br><br> <a href="/">Return to USBODE homepage</a>'
+    return f'Switching mode... Current mode is {checkState()} (1=CD-Emulator, 2=ExFAT mode)<br><br><a href="/switch">Need to switch modes again?</a><br><br> <a href="/">Return to USBODE homepage</a>'
 @app.route('/list')
 def listFiles():
     fileList=list_images()
@@ -99,16 +92,6 @@ def exit():
     start_exit()
     Thread.is_alive == 0
     return f"Exiting the app now"
-@app.route('/setup')
-def setup():
-    fileList=list_images()
-    if len(fileList) < 1:
-        return f"No images found in {store_mnt}. Please add at least one ISO image and try again. <br>Adding images requires ExFAT support, connect this device to a system that supports ExFAT, then <a href='/switch'> switch modes</a> <br><br><a href='/shutdown'>Shutdown the pi</a>"
-    else:
-        response=""
-        for file in fileList:
-            response+=f"<a href='/mount/{file}'>{file}</a><br><br>"
-        return f"Current File Loaded: NONE (First Setup!)<br><br>To load a different ISO, select it. Be aware the system will disconnect and reconnect the optical drive.<br><br> {response} <br> <a href='/'>Return to USBODE homepage</a>"
 
 def getMountedCDName():
     if not os.path.exists(gadgetCDFolder+"/functions/mass_storage.usb0/lun.0/file"):
@@ -158,7 +141,6 @@ def init_gadget(type):
         subprocess.run(['sh', 'scripts/exfat_gadget_setup.sh', gadgetCDFolder], cwd="/opt/usbode")
         change_Loaded_Mount(f"{store_dev}")
     enable_gadget()
-
 
 def enable_gadget():
     p = subprocess.run(['sh', 'scripts/enablegadget.sh', gadgetCDFolder], cwd="/opt/usbode")
@@ -238,18 +220,6 @@ def change_Loaded_Mount(filename):
         global updateEvent
         updateEvent = 1
         return True
-
-# Help information
-help_cmds = [
-    ['help', 'Displays this message'],
-    ['version', 'Displays version info'],
-    ['exit', 'Terminate the script'],
-    ['shutdown', 'Shuts down the Pi'],
-    ['disable', 'Disables the gadget'],
-    ['mode', 'Get current mode'],
-    ['switch [mode]', 'Switch to a specified mode (1=cdrom, 2=store)'],
-    ['switch', 'Switch to the other mode (toggle like the hardware button)'],
-]
 
 def start_exit():
     global disp
@@ -436,35 +406,6 @@ def main():
         oledDaemon.start()
     while exitRequested == 0:
         pass
-        # cmd = input("usbode> ")
-        # cmd = cmd.strip(' ')
-        # cmd_args = cmd.split(' ')
-        # cmd = cmd_args[0]
-        # if cmd == 'help':
-        #     for help_cmd in help_cmds:
-        #         if len(help_cmd) == 0:
-        #             continue
-        #         else:
-        #             print(f"{help_cmd[0]}", end='')
-        #             if len(help_cmd) == 2:
-        #                 indent = '\t\t' if (len(help_cmd[0]) < 8) else '\t'
-        #                 print(f"{indent}{help_cmd[1]}")
-        #             if len(help_cmd) > 2:
-        #                 for i in range(2,len(help_cmd)):
-        #                     print(f"\t\t{help_cmd[i]}")
-        # elif cmd == 'version':
-        #     version()
-        # elif cmd == 'exit':
-        #     start_exit()
-        #     quit(0)
-        # elif cmd == 'shutdown':
-        #     start_exit()
-        #     start_shutdown()
-        # elif cmd == 'switch':
-        #     switch()
-        # else:
-        #     if cmd.strip(' ') != '':
-        #         print(f"Invalid command: {cmd}")  
     start_exit()
     quit(0)
 
