66dd41e3f32c1609112afdb1339673685c054330
Dani Sarfati <dani@funkervogt.com>
2025-05-05 09:34:10 -0400

    final changes for v1.9

--- inst/usbode/scripts/cd_gadget_setup.sh
+++ inst/usbode/scripts/cd_gadget_setup.sh
@@ -1,13 +1,13 @@
 cd $1
 
 echo "0x04da" > idVendor  # Panasonic
-echo "0xd01" > idProduct # USB CD-ROM Drive KXL-840AN (increases compability with many retro systems)
+echo "0x0d01" > idProduct # USB CD-ROM Drive KXL-840AN (increases compability with many retro systems)
 echo 0x0100 > bcdDevice   # v1.0.0
 echo 0x0200 > bcdUSB      # USB 2.0
 
 echo "1111111111" > strings/0x409/serialnumber
 echo "Linux" > strings/0x409/manufacturer
-echo "USBODE-v1.8" > strings/0x409/product
+echo "USBODE-v1.9" > strings/0x409/product
 
 echo "Config 1: USBODE" > configs/c.1/strings/0x409/configuration
 echo 0 > configs/c.1/MaxPower
